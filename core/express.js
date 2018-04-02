2f41377c49384c9d8769deafa10ab94507fe8a4c
NCharvy <nathan.charvy@gmail.com>
2018-04-03 00:16:52 +0200

    move base route

--- core/express.js
+++ core/express.js
@@ -7,4 +7,16 @@ let App = express();
 App.use(bodyParser.json());
 App.use(cors());
 
+App.get("/", (req, res) => {
+	res.json({
+		data: {
+			title: "Story Engine",
+			description: "A Node.js based engine to create non-linear stories",
+			author: "Nythe",
+			license: "MIT",
+			readme: "https://github.com/StoryTailor/storyengine#readme"
+		}
+	});
+});
+
 module.exports = App;
--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const path = require("path");
 const express = require(path.join(__dirname, "core/express.js"));
 const http = require("http");
 const server = http.createServer(express);
+const port = 27000;
 
-server.listen(27000);
+server.listen(port);
+console.log(`Running on port ${port}`);
