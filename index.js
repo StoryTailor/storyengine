80109caac0dd99f3bf575dd999836a55a3e86905
Nythe <nathan.charvy@gmail.com>
2018-04-17 22:05:45 +0200

    bundles implicitly loaded by the core

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const App = require(path.join(__dirname, "src"));
-//let bundles = require(path.join(__dirname, "src/bundles"));
 
 // Starts the engine
-App.startEngine(bundles);
+App.startEngine();
