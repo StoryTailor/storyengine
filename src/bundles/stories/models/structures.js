88361d0dab24f9e7e351ba5950baae402c58b350
Nythe <nathan.charvy@gmail.com>
2018-05-14 22:20:11 +0200

    change structure to validate mixed children

--- src/bundles/stories/models/structures.js
+++ src/bundles/stories/models/structures.js
@@ -1,5 +1,5 @@
 const Schema = require("mongoose").Schema;
-const Base = require("./base/baseStructure.js");
+const structure = require("./validation/structure.json");
 
 /**
 * Schema Structures
@@ -7,11 +7,70 @@ const Base = require("./base/baseStructure.js");
 class Structures extends Schema {
     constructor() {
         super({
-            base : {
-                type : Base
+            content : {
+                type : String,
+                required : true
             },
-            children : [Base]
+            choices : [{
+                label : {
+                    type : String,
+                    required : true
+                },
+                subStep : {
+                    type : Number,
+                    required : true
+                }
+            }],
+            step : {
+                type : Number,
+                required : true
+            },
+            background : {
+                type : String
+            }
         });
+
+        this.add({
+            children : {
+                type: Schema.Types.Mixed,
+                validate: {
+                    validator: (elem) => {
+                        if(Array.isArray(elem)){
+                            if(elem.length > 0){
+                                this._validateChildren(elem);
+                            }
+                        }
+                        else {
+                            throw new Error("The children format mismatch with the requested one");
+                        }
+
+                        return true;
+                    }
+                }
+            }
+        })
+    }
+
+    /**
+     * Validation method to check the right format
+     * of the children structure
+     * 
+     * @method  _validateChildren
+     * @param  {Array}  elem    Children structure     
+     * @return {void}
+     */
+    _validateChildren(elem) {
+        for (let i in elem) {
+            let child = elem[i];
+            for(let field in child) {
+                if(!structure.hasOwnProperty(field) && field !== "children") {
+                    throw new Error(`Validation error for children structure : ${field} doesn\'t exists`);
+                }
+                else if(Array.isArray(child[field]) && field === "children") {
+                    this._validateChildren(child[field]);
+                }
+            }
+        }
     }
 }
 
